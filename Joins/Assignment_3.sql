92360c4da36b2b7e5ac506286447c4bf43229876
ManojKumar2301 <manojkumarpendekanti23@gmail.com>
2025-02-10 00:20:59 +0530

    Update Assignment_3.sql

--- Joins/Assignment_3.sql
+++ Joins/Assignment_3.sql
@@ -1,1 +1,5 @@
-
+-- Assignment 3: Using RIGHT JOIN
+-- Write a RIGHT JOIN query to list all courses along with the students enrolled. Ensure courses with no students also appear.
+SELECT students.student_id, students.name, courses.course_name
+FROM students
+RIGHT JOIN courses ON students.student_id = courses.student_id;
