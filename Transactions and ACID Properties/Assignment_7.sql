52a18af0152ce445460a8e9170b6b8fa156ce162
ManojKumar2301 <manojkumarpendekanti23@gmail.com>
2025-02-10 00:25:13 +0530

    Update Assignment_7.sql

--- Transactions and ACID Properties/Assignment_7.sql
+++ Transactions and ACID Properties/Assignment_7.sql
@@ -1,1 +1,33 @@
+-- Assignment 7: Banking Transactions
+-- Create a bank_accounts(account_id, customer_name, balance) table. 
+CREATE TABLE bank_accounts (
+    account_id SERIAL PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    balance DECIMAL(10, 2) CHECK (balance >= 0)
+);
+
+-- Insert values in bank_accounts
+INSERT INTO bank_accounts (customer_name, balance) VALUES
+('Manoj', 1000.00),
+('Koti', 500.00), 
+('Vinay', 700.00);
+
+SELECT * FROM bank_accounts;
+
+BEGIN;
+
+-- Deduct $100 from one account.
+UPDATE bank_accounts 
+SET balance = balance - 100 
+WHERE account_id = 1;
+
+-- Add $100 to another.
+UPDATE bank_accounts 
+SET balance = balance + 100 
+WHERE account_id = 2;
+
+COMMIT;
+
+-- Ensure both updates occur inside a transaction.
+SELECT * FROM bank_accounts;
 
